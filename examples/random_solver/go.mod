ddd17e38768b42e14c0f223dc52850a4ee932184
Takeru Ohta <phjgt308@gmail.com>
2019-12-25 22:10:57 +0900

    Add examples

---
+++ examples/random_solver/go.mod
@@ -0,0 +1,7 @@
+module github.com/sile/kurobako-go/examples/random_solver
+
+go 1.13
+
+require github.com/sile/kurobako-go/kurobako v0.0.0-20191224165402-bd75a2252acc
+
+replace github.com/sile/kurobako-go/kurobako => ../../kurobako
---
+++ examples/random_solver/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"github.com/sile/kurobako-go/kurobako"
+	"math"
+	"math/rand"
+)
+
+type randomSolverFactory struct{}
+
+func (r *randomSolverFactory) Specification() (*kurobako.SolverSpec, error) {
+	spec := kurobako.NewSolverSpec("Random Search")
+	return &spec, nil
+}
+
+func (r *randomSolverFactory) CreateSolver(seed int64, problem kurobako.ProblemSpec) (kurobako.Solver, error) {
+	rng := rand.New(rand.NewSource(seed))
+	return &randomSolver{rng, problem}, nil
+}
+
+type randomSolver struct {
+	rng     *rand.Rand
+	problem kurobako.ProblemSpec
+}
+
+func (r *randomSolver) sampleUniform(low float64, high float64) float64 {
+	return r.rng.Float64()*(high-low) + low
+}
+
+func (r *randomSolver) sampleLogUniform(low float64, high float64) float64 {
+	return math.Exp(r.sampleUniform(math.Log(low), math.Log(high)))
+}
+
+func (r *randomSolver) Ask(idg *kurobako.TrialIdGenerator) (kurobako.NextTrial, error) {
+	var trial kurobako.NextTrial
+
+	for _, p := range r.problem.Params {
+		if p.Distribution == kurobako.Uniform {
+			value := r.sampleUniform(p.Range.Low(), p.Range.High())
+			trial.Params = append(trial.Params, &value)
+		} else {
+			value := r.sampleLogUniform(p.Range.Low(), p.Range.High())
+			trial.Params = append(trial.Params, &value)
+		}
+	}
+
+	trial.TrialId = idg.Generate()
+	trial.NextStep = r.problem.Steps.Last()
+	return trial, nil
+}
+
+func (r *randomSolver) Tell(trial kurobako.EvaluatedTrial) error {
+	return nil
+}
+
+func main() {
+	runner := kurobako.NewSolverRunner(&randomSolverFactory{})
+	if err := runner.Run(); err != nil {
+		panic(err)
+	}
+}
