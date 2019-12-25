d2e1ae082e00c9d8e6fa2a2dc48fccbe7574025a
Takeru Ohta <phjgt308@gmail.com>
2019-12-25 22:10:08 +0900

    Update problem.go

--- kurobako/problem.go
+++ kurobako/problem.go
@@ -3,10 +3,13 @@ package kurobako
 import (
 	"bufio"
 	"encoding/json"
+	"errors"
 	"fmt"
-	"os"
 	"io"
+	"os"
 )
+
+var UnevalableParamsError = errors.New("unevalable params")
 
 type ProblemSpec struct {
 	Name   string            `json:"name"`
@@ -14,6 +17,15 @@ type ProblemSpec struct {
 	Params []Var             `json:"params_domain"`
 	Values []Var             `json:"values_domain"`
 	Steps  Steps             `json:"steps"`
+}
+
+func NewProblemSpec(name string) ProblemSpec {
+	steps, _ := NewSteps([]uint64{1})
+	return ProblemSpec{
+		Name:  name,
+		Attrs: map[string]string{},
+		Steps: *steps,
+	}
 }
 
 type Evaluator interface {
@@ -25,7 +37,7 @@ type Problem interface {
 }
 
 type ProblemFactory interface {
-	Specification() (ProblemSpec, error)
+	Specification() (*ProblemSpec, error)
 	CreateProblem(seed int64) (Problem, error)
 }
 
@@ -50,7 +62,7 @@ func (r *ProblemRunner) Run() error {
 	for {
 		do_continue, err := r.runOnce()
 		if err != nil {
-			return nil
+			return err
 		}
 
 		if !do_continue {
@@ -76,20 +88,115 @@ func (r *ProblemRunner) runOnce() (bool, error) {
 
 	switch message["type"] {
 	case "CREATE_PROBLEM_CAST":
-		panic("todo")
+		err := r.handleCreateProblemCast(line)
+		return true, err
 	case "DROP_PROBLEM_CAST":
-		panic("todo")
+		err := r.handleDropProblemCast(line)
+		return true, err
 	case "CREATE_EVALUATOR_CALL":
-		panic("todo")
-	case "DROP_EVALUATOR_CALL":
-		panic("todo")
+		err := r.handleCreateEvaluatorCall(line)
+		return true, err
+	case "DROP_EVALUATOR_CAST":
+		err := r.handleDropEvaluatorCast(line)
+		return true, err
 	case "EVALUATE_CALL":
-		panic("todo")
+		err := r.handleEvaluateCall(line)
+		return true, err
 	default:
 		return false, fmt.Errorf("unknown message type: %v", message["type"])
 	}
+}
 
-	return true, nil
+func (r *ProblemRunner) handleEvaluateCall(input []byte) error {
+	var message struct {
+		EvaluatorId uint64 `json:"evaluator_id"`
+		NextStep    uint64 `json:"next_step"`
+	}
+
+	if err := json.Unmarshal(input, &message); err != nil {
+		return err
+	}
+
+	evaluator := r.evaluators[message.EvaluatorId]
+	currentStep, values, err := evaluator.Evaluate(message.NextStep)
+	if err != nil {
+		return err
+	}
+
+	reply := map[string]interface{}{
+		"type":         "EVALUATE_REPLY",
+		"current_step": currentStep,
+		"values":       values,
+	}
+	return r.sendMessage(reply)
+}
+
+func (r *ProblemRunner) handleDropEvaluatorCast(input []byte) error {
+	var message struct {
+		EvaluatorId uint64 `json:"evaluator_id"`
+	}
+
+	if err := json.Unmarshal(input, &message); err != nil {
+		return err
+	}
+
+	delete(r.evaluators, message.EvaluatorId)
+	return nil
+}
+
+func (r *ProblemRunner) handleCreateEvaluatorCall(input []byte) error {
+	var message struct {
+		ProblemId   uint64    `json:"problem_id"`
+		EvaluatorId uint64    `json:"evaluator_id"`
+		Params      []float64 `json:"params"`
+	}
+
+	if err := json.Unmarshal(input, &message); err != nil {
+		return err
+	}
+
+	problem := r.problems[message.ProblemId]
+	evaluator, err := problem.CreateEvaluator(message.Params)
+	if err == UnevalableParamsError {
+		return r.sendMessage(map[string]interface{}{"type": "ERROR_REPLY", "kind": "UNEVALABLE_PARAMS"})
+	} else if err != nil {
+		return err
+	}
+
+	r.evaluators[message.EvaluatorId] = evaluator
+	return r.sendMessage(map[string]interface{}{"type": "CREATE_EVALUATOR_REPLY"})
+}
+
+func (r *ProblemRunner) handleDropProblemCast(input []byte) error {
+	var message struct {
+		ProblemId uint64 `json:"problem_id"`
+	}
+
+	if err := json.Unmarshal(input, &message); err != nil {
+		return err
+	}
+
+	delete(r.problems, message.ProblemId)
+	return nil
+}
+
+func (r *ProblemRunner) handleCreateProblemCast(input []byte) error {
+	var message struct {
+		ProblemId  uint64 `json:"problem_id"`
+		RandomSeed uint64 `json:"random_seed"`
+	}
+
+	if err := json.Unmarshal(input, &message); err != nil {
+		return err
+	}
+
+	problem, err := r.factory.CreateProblem(int64(message.RandomSeed))
+	if err != nil {
+		return err
+	}
+
+	r.problems[message.ProblemId] = problem
+	return nil
 }
 
 func (r *ProblemRunner) castProblemSpec() error {
