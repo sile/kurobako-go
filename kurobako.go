9dc6f055ce2f75f024d2a88100290fbe431f8a95
Takeru Ohta <phjgt308@gmail.com>
2020-01-05 16:49:50 +0900

    Add kurobako.go file

---
+++ kurobako.go
@@ -0,0 +1,7 @@
+// A library to help implement [kurobako]'s solvers and problems.
+//
+// This package also provides a [Goptuna integration].
+//
+// [kurobako]: https://github.com/sile/kurobako
+// [Goptuna integration]: https://godoc.org/github.com/sile/kurobako-go/goptuna/solver
+package kurobako
