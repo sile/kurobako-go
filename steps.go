b49f047cc1198ae930d754eed9007cb8296bdd5f
Takeru Ohta <phjgt308@gmail.com>
2020-01-05 19:38:59 +0900

    Add doc to steps.go file

--- steps.go
+++ steps.go
@@ -5,11 +5,15 @@ import (
 	"fmt"
 )
 
+// Steps represents a sequence of evaluable steps of a problem.
+//
+// At each evaluable step, the evaluator of the problem can suspend the evaluation and return intermediate evaluation values at that step.
 type Steps struct {
 	isSequential bool
 	steps        []uint64
 }
 
+// NewSteps creates a new Steps instance.
 func NewSteps(steps []uint64) (*Steps, error) {
 	if len(steps) == 0 {
 		return nil, fmt.Errorf("empty steps isn't allowed")
@@ -33,10 +37,12 @@ func NewSteps(steps []uint64) (*Steps, error) {
 	return &Steps{false, steps}, nil
 }
 
+// Last returns the last step.
 func (r Steps) Last() uint64 {
 	return r.steps[len(r.steps)-1]
 }
 
+// AsSlice returns the steps as slice.
 func (r Steps) AsSlice() []uint64 {
 	if r.isSequential {
 		last := r.Last()
@@ -50,6 +56,7 @@ func (r Steps) AsSlice() []uint64 {
 	return r.steps
 }
 
+// MarshalJSON encodes a Steps object to JSON bytes.
 func (r Steps) MarshalJSON() ([]byte, error) {
 	if r.isSequential {
 		return json.Marshal(r.Last())
@@ -58,6 +65,7 @@ func (r Steps) MarshalJSON() ([]byte, error) {
 	return json.Marshal(r.steps)
 }
 
+// UnmarshalJSON decodes a Steps object from JSON bytes.
 func (r *Steps) UnmarshalJSON(data []byte) error {
 	var lastStep uint64
 	if json.Unmarshal(data, &lastStep) == nil {
